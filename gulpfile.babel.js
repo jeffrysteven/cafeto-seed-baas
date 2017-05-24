830dcab6d16d769c03e52e5a5496952e9682432d
Jeffry Steven Lenis Jimenez <jeffry_steven@MacBook-Pro-de-Jeffry-2.local>
2017-05-23 21:49:31 -0500

    CB-04: Gulp improvements.
    * ES6 Gulp improvements.
    * Parse configuration improvements.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,46 +9,28 @@ import watch from 'gulp-watch';
 import server from 'gulp-live-server';
 import babel from 'gulp-babel';
 
+let express: any;
+
 const paths = {
     src: [Config.PROJECT_ROOT + '/src/**'],
     js: [Config.PROJECT_ROOT + '/src/**/*.js'],
     destination: Config.PROJECT_ROOT + '/dist'
 }
 
-gulp.task('default', (cb: any) => {
-    run('lint', cb);
-});
+gulp.task('serve.dev', (cb: any) => run('server', 'build', 'watch', cb));
 
-gulp.task('serve.dev', (cb: any) => {
-    run('server', 'build', 'watch', cb);
-});
+gulp.task('serve.prod', (cb: any) => run('server', 'build', cb));
 
-gulp.task('serve.prod', (cb: any) => {
-    run('server', 'build', cb);
-});
+gulp.task('build', (cb: any) => run('clean', 'flow', 'babel', 'restart', cb));
 
-gulp.task('build', (cb: any) => {
-    run('clean', 'flow', 'babel', 'restart', cb);
-});
+gulp.task('clean', (cb: any) => rimraf(paths.destination, cb));
 
-gulp.task('clean', (cb: any) => {
-    rimraf(paths.destination, cb);
-});
-
-gulp.task('flow', shell.task([
-    'flow'
-], { ignoreErrors: true }));
+gulp.task('flow', () => shell.task(['flow'], { ignoreErrors: true }));
 
 gulp.task('babel', () => gulp.src(paths.js).pipe(babel()).pipe(gulp.dest('dist')));
 
-let express: any;
+gulp.task('server', () => { express = server.new(paths.destination) });
 
-gulp.task('server', () => express = server.new(paths.destination));
+gulp.task('restart', () => { express.start.bind(express)() });
 
-gulp.task('restart', () => express.start.bind(express)());
-
-gulp.task('watch', () => {
-    return watch(paths.js, () => {
-        gulp.start('build');
-    });
-});
+gulp.task('watch', () => watch(paths.js, () => { gulp.start('build') }));
--- src/index.js
+++ src/index.js
@@ -3,24 +3,39 @@
 import express from 'express';
 import { ParseServer } from 'parse-server';
 import ParseDashboard from 'parse-dashboard';
+import path from 'path';
 
-const app = express();
+const SERVER_PORT = process.env.PORT || 8080;
+const SERVER_HOST = process.env.HOST || 'localhost';
+const APP_ID = process.env.APP_ID || 'cafeto-baas-app';
+const MASTER_KEY = process.env.MASTER_KEY || 'Cafeto2010';
+const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://localhost:27017/dev';
+const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
+const DASHBOARD_AUTH = process.env.DASHBOARD_AUTH;
 
-const api = new ParseServer({
-    databaseURI: 'mongodb://localhost:27017/cafetoTest',
-    //cloud: '/home/myApp/cloud/main.js', // Absolute path to your Cloud Code
-    appId: 'cafetobaas',
-    masterKey: 'Cafeto2010', // Keep this key secret!
-    fileKey: 'Cafeto2010',
-    serverURL: 'http://localhost:1337/api'
+const app: any = express();
+
+const api: ParseServer = new ParseServer({
+    databaseURI: DATABASE_URI,
+    cloud: path.resolve(__dirname, 'cloud.js'),
+    appId: APP_ID,
+    masterKey: MASTER_KEY,
+    fileKey: MASTER_KEY,
+    serverURL: `http://${SERVER_HOST}:${SERVER_PORT}/api`,
+    push: {
+        android: {
+            senderId: '805651505507',
+            apiKey: 'AIzaSyAMsJM4UUaXzFR9ytRtzQZr6twOqOapZbw'
+        }
+    }
 });
 
-const dashboard = new ParseDashboard({
+const dashboard: ParseDashboard = new ParseDashboard({
     "apps": [
         {
-            appId: 'cafetobaas',
-            masterKey: 'Cafeto2010',
-            serverURL: 'http://localhost:1337/api',
+            appId: APP_ID,
+            masterKey: MASTER_KEY,
+            serverURL: `http://${SERVER_HOST}:${SERVER_PORT}/api`,
             appName: 'HeadsUp'
         }
     ]
@@ -29,4 +44,6 @@ const dashboard = new ParseDashboard({
 app.use('/api', api);
 app.use('/dashboard', dashboard);
 
-app.listen(1337, () => console.log('Cafeto parse server running on port 1337.'));
+app.listen(SERVER_PORT, () => console.log(
+    `Server is now running in ${process.env.NODE_ENV || 'development'} mode on http://${SERVER_HOST}:${SERVER_PORT}`)
+);
